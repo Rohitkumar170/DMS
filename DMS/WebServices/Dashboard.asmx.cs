d17ef518e2edf9b5240f39bf498b63313f608da3
Rohit kumar <rohityoekisoft@gmail.com>
2020-02-07 04:43:23 -0800

    Complete solution

---
+++ DMS/WebServices/Dashboard.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace DealerManagementSystem.WebServices
+{
+    /// <summary>
+    /// Summary description for Dashboard
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+     [System.Web.Script.Services.ScriptService]
+    public class Dashboard : System.Web.Services.WebService
+    {
+
+        [WebMethod(EnableSession = true)]
+        public void Logout()
+        {
+
+            System.Web.HttpContext.Current.Session["UserID"] = null;
+            Session.Abandon();
+            Session.Clear();
+
+
+        }
+    }
+}
