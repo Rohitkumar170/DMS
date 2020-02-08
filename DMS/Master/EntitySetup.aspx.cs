2f19e7e755ef31c22dc6d18399e6b2c273e0d971
Rohit kumar <rohityoekisoft@gmail.com>
2020-02-08 02:04:28 -0800

    ImportClass

--- DMS/Master/EntitySetup.aspx.cs
+++ DMS/Master/EntitySetup.aspx.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.OleDb;
+using System.IO;
+using System.Data;
 namespace DMS.Master
 {
     public partial class EntitySetup : System.Web.UI.Page
@@ -13,5 +15,37 @@ namespace DMS.Master
         {
             lblUserId.InnerText = Convert.ToString(Session["UserId"]);
         }
+
+        //public void change()
+        //{
+        //    string postedFile;
+        //    string filePath = string.Empty;
+        //    if (postedFile != null)
+        //    {
+        //        string path = Server.MapPath("~/Uploads/");
+        //        if (!Directory.Exists(path))
+        //        {
+        //            Directory.CreateDirectory(path);
+        //        }
+        //        string extension = Path.GetExtension(postedFile.FileName);
+        //        string conString = string.Empty;
+        //        switch (extension)
+        //        {
+        //            case ".xls": //Excel 97-03.
+        //                conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+        //                break;
+        //            case ".xlsx": //Excel 07 and above.
+        //                conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+        //                break;
+        //        }
+
+        //        DataTable dt = new DataTable();
+        //        conString = string.Format(conString, filePath);
+
+
+
+        //    }
+
+        //}
     }
 }
---
+++ DMS/Models/EntitySetupClass/ImportEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMS.Models.EntitySetupClass
+{
+    public class ImportEntity
+    {
+        public string Entityname { get; set; }
+        public Int64 CountryId { get; set; }
+        public Int64 MobileNo { get; set;}
+        public string EmailId { get; set; }
+        public Int64 CreatedBy { get; set;}
+
+    }
+}
