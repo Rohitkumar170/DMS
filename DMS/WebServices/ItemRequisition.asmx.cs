fe5d1d776d9d0f61ecfa93c85e4403ebb508141a
Rohit kumar <rohityoekisoft@gmail.com>
2020-02-29 12:30:02 +0530

    space

--- DMS/WebServices/ItemRequisition.asmx.cs
+++ DMS/WebServices/ItemRequisition.asmx.cs
@@ -15,7 +15,7 @@ namespace DMS.WebServices
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    [System.ComponentModel.ToolboxItem(false)]
+    [System.ComponentModel.ToolboxItem(false)] 
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
      [System.Web.Script.Services.ScriptService]
     public class ItemRequisition : System.Web.Services.WebService
